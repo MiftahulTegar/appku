029d1db4d46ac133605743a140125ecbe76c862a
miftahul tegar <tegarpangestu117@gmail.com>
2024-06-13 16:00:03 +0700

    add

--- main.py
+++ main.py
@@ -3,4 +3,5 @@ from website import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True, threaded=True)
+    app.run(debug=True)
+    # app.run(debug=True, host='0.0.0.0', port=5506, threaded=True)
